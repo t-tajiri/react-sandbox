49c822fd0bb6913ea19ed6e2bffb4f83c4e9ab65
t-tajiri <swallow.ava@gmail.com>
2019-06-01 17:48:27 +0900

    refactor class component to functional component

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import './App.css'
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/cat">cat</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    )
-  }
+const Header = () => {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/cat">cat</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
 
 export default Header
