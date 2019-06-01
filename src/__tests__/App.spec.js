2683225935adb1089cb9fc1f7494ed5866c0db2f
t-tajiri <swallow.ava@gmail.com>
2019-06-01 18:19:20 +0900

    eject using library and extract duplicate setup code

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import { shallow, configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import { shallow } from 'enzyme'
 import App from '../App'
 
-configure({ adapter: new Adapter() })
 describe('App.js', () => {
   let wrapper
 
--- src/__tests__/Header.spec.js
+++ src/__tests__/Header.spec.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { shallow, configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import { shallow } from 'enzyme'
 import Header from '../Header'
-
-configure({ adapter: new Adapter() })
 
 describe('Header.js', () => {
   let wrapper
