486da4b343998dc5af3d8aa1d6a99c073dfeeda4
t-tajiri <swallow.ava@gmail.com>
2019-06-26 21:59:31 +0900

    add Todo component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const todo = () => {
-  return <p>TODO</p>
+const Todo = ({ onClick, completed, text }) => {
+  return (
+    <li
+      onClick={ onClick }
+      style={{
+        textDecoration: completed ? 'line-through' : 'none'
+      }}
+    >
+      text
+    </li>
+  )
 }
 
-export default todo
+Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired
+}
+
+export default Todo
